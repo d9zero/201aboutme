e609c8ed33e5bf68bba26082823848d450b9441d
LeeThomas13 <lmtthomasritz@gmail.com>
2020-08-06 16:39:48 -0700

    Made a new if statement and added funtions to all new and previous if statements.

--- aboutme2game.js
+++ aboutme2game.js
@@ -1,37 +1,63 @@
 "use strict";
 
-var wanttoplayagame = prompt("Do you want to play a game?");
+playGame();
 
-console.log("does the user want to play a game? " + wanttoplayagame);
+drinkQuestion();
 
-//  if (condition is true){
-//     do this thing} else {
-//       do this other thing
-//    }
+function playGame(){
+  var wanttoplayagame = prompt("Do you want to play a game?");
 
-//  if(condition is true){
-//   do this thing
-//  }
+  console.log("does the user want to play a game? " + wanttoplayagame);
 
-// if (condition is true){
-//   do this thing} else if(this other condition is true){
-//   do another thing }
+  //  if (condition is true){
+  //     do this thing} else {
+  //       do this other thing
+  //    }
 
-if(wanttoplayagame.toLowerCase() === 'yes' || wanttoplayagame === 'sure' || wanttoplayagame === "ya" || wanttoplayagame === "y" || wanttoplayagame === "yea" ){
-  alert("Lets begin...");}
-  else if(wanttoplayagame.toLowerCase() === 'no' || wanttoplayagame === "n" || wanttoplayagame === "nope" || wanttoplayagame === "na"){
-    alert("Perhaps you will ready one day.");}
-    else {
-      alert("Please enter yes or no for the following questions");}
+  //  if(condition is true){
+  //   do this thing
+  //  }
 
-if(wanttoplayagame.toUpperCase() === "Yes" || wanttoplayagame === "Sure" || wanttoplayagame === "Ya" || wanttoplayagame === "Y" || wanttoplayagame === "Yea"){
-  alert("Lets begin...");}
-  else if(wanttoplayagame.toUpperCase() === "No" || wanttoplayagame === "N" || wanttoplayagame === "Nope" || wanttoplayagame === "Na"){
-    alert("Perhaps you will be ready one day.");}
-    else {
-      alert("Please enter yes or no for the following questions");}
-    
-// round 2
+  // if (condition is true){
+  //   do this thing} else if(this other condition is true){
+  //   do another thing }
 
+  if(wanttoplayagame.toLowerCase() === 'yes' || wanttoplayagame === 'sure' || wanttoplayagame === "ya" || wanttoplayagame === "y" || wanttoplayagame === "yea" ){
+    alert("Lets begin...");}
+    else if(wanttoplayagame.toLowerCase() === 'no' || wanttoplayagame === "n" || wanttoplayagame === "nope" || wanttoplayagame === "na"){
+      alert("Perhaps you will ready one day.");}
+      else {
+        alert("Please enter yes or no for the following questions");}
 
+  if(wanttoplayagame.toUpperCase() === "Yes" || wanttoplayagame === "Sure" || wanttoplayagame === "Ya" || wanttoplayagame === "Y" || wanttoplayagame === "Yea"){
+    alert("Lets begin...");}
+    else if(wanttoplayagame.toUpperCase() === "No" || wanttoplayagame === "N" || wanttoplayagame === "Nope" || wanttoplayagame === "Na"){
+      alert("Perhaps you will be ready one day.");}
+      else {
+        alert("Please enter yes or no for the following questions");}
+  // round 2
+  }
 
+function drinkQuestion() {
+
+  var favDrinkAnswer = ['Long Island','Mac & Jacks','AMF','Tequila & Lime','BSB']
+
+  for (var i=0; i<6; i++){
+    var favoriteDrink = prompt('Can you guess what my favorite drink is?');
+    var answerCorrect = false
+
+    for (var p = 0; p < favDrinkAnswer.length; p++){
+      if (favDrinkAnswer[p].toLowerCase() === favoriteDrink.toLowerCase()){
+        alert('You\'re correct! That is one of my favorite drinks!');
+        answerCorrect = true
+      }
+    }
+    if (answerCorrect === true){
+      break;
+    } else {
+      alert('You are incorrect, please try again');
+    } if (i === 6){
+      alert('You are out of guesses, I dont drink that stuff');
+    }
+  }
+}
